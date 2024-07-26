f01071a6b10bc6e5f34db8bc419e0b5dce711b94
Josenilton Anjos <joseniltonsilva040@gmail.com>
2024-07-26 17:56:02 -0300

    t

--- script.js
+++ script.js
@@ -38,26 +38,3 @@ uniqueImageUrls.forEach(imageUrl => {
   scrollingWrapper.appendChild(imgElement);
 });
   
-// Inicialize o EmailJS com seu User ID
-emailjs.init('ccL1GweldbY_h9zrOeXu5');
-
-// Adicione um ouvinte de eventos ao botão
-document.getElementById('send-button').addEventListener('click', function() {
-    var message = document.getElementById('message-box').value.trim();
-
-    if (message !== '') {
-        // Enviar o e-mail usando o EmailJS
-        emailjs.send('service_kq5vte6', 'template_01', {
-            message: message
-        })
-        .then(function(response) {
-            document.getElementById('response-message').textContent = 'Pedido de oração enviado com sucesso!';
-            document.getElementById('message-box').value = ''; // Limpa a caixa de mensagem
-        }, function(error) {
-            document.getElementById('response-message').textContent = 'Erro ao enviar pedido. Tente novamente.';
-            console.error('Erro ao enviar e-mail:', error);
-        });
-    } else {
-        document.getElementById('response-message').textContent = 'Por favor, digite um pedido de oração.';
-    }
-});
